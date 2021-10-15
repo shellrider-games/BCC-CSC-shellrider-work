31b0ae634dd30550b027e7e3a02fe08e63cd165f
Becker Georg <georg.becker@shellrider.com>
2021-10-15 14:35:22 +0200

    Add variables and console statements

--- 20211015/script.js
+++ 20211015/script.js
@@ -1,1 +1,16 @@
 console.log('Application starts!');
+
+let age = 27;
+let userName = 'shellrider';
+let id = '1234';
+
+console.log(`User: ${userName} with ID: ${id} logged in. Users age is ${age} years old.`);
+
+const bccYear = 2021;
+
+console.log(`Type of age is: ${typeof age}`);
+console.log(`Type of userName is: ${typeof userName}`);
+console.log(`Type of id is: ${typeof id}`);
+
+console.log(`Type of ${true} is ${typeof true}`);
+console.log(`Type of ${false} is ${typeof false}`);
