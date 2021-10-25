14b0ed2500f253bb08ec51c15f603db57ea25dbe
Becker Georg <cc211007@fhstp.ac.at>
2021-10-25 13:56:59 +0200

    Add joulupukki and todays code

---
+++ 20211025/script.js
@@ -0,0 +1,38 @@
+//Modify and assign
+let answerToEverything = 42;
+
+console.log(answerToEverything + 8);
+console.log(answerToEverything);
+
+answerToEverything += 8; // answerToEverything = answerToEverything + 8
+
+//Increment and Decrement
+let counter = 0;
+console.log(counter++); // logs 0
+console.log(++counter); // logs 2
+
+console.log('========LOGICAL OPERATORS=========');
+
+//AND
+let result = 2 > 1 && 3 < 4;
+console.log(result);
+
+//OR
+result = 2 > 1 || 3 > 4;
+console.log(result);
+
+result = !(2 > 1 && 3 < 4);
+console.log(result);
+
+console.log('========COMPARISION OPERATORS=========');
+
+console.log(1 == 1);
+
+console.log('1' == 1);
+console.log('1' === 1);
+
+console.log(2 != '2');
+console.log(2 !== '2');
+
+console.log(2 >= 2);
+console.log(3 <= 3);
