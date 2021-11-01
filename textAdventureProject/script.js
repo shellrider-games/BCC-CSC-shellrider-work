d421e0f8cdbd5dac056e6a412af91c250112bc93
Georg Becker <Georg.Becker@gmx.at>
2021-11-01 15:24:53 +0100

    Add functionality for going back

--- textAdventureProject/script.js
+++ textAdventureProject/script.js
@@ -2,6 +2,9 @@ const textDiv = document.getElementById("textContainer");
 const buttonDiv = document.getElementById("choiceButtons");
 const imageElement = document.getElementById("backgroundImageElement");
 const backButton = document.getElementById("backBtn");
+backButton.onclick = lastNode;
+
+const nodeHistory = [];
 
 class Choice {
     constructor(text, node = null) {
@@ -21,7 +24,7 @@ class StoryNode {
 function appendChoice(choice){
     let button = document.createElement("button");
     button.innerHTML = choice.text;
-    button.onclick = function () {if (choice.node) {renderNode(choice.node)}};
+    button.onclick = function () {if (choice.node) {nextNode(choice.node)}};
     buttonDiv.appendChild(button);
 }
 
@@ -31,8 +34,22 @@ function renderNode(node) {
     for (const choice of node.choices) {
         appendChoice(choice);
     }
-    buttonDiv.appendChild(backButton);
+    if(nodeHistory.length > 1) {
+        buttonDiv.appendChild(backButton);
+    }
     imageElement.setAttribute("src",node.imagePath);
+}
+
+function nextNode(node) {
+    nodeHistory.push(node);
+    renderNode(node);
+}
+
+function lastNode() {
+    if(nodeHistory.length > 1){
+        nodeHistory.pop();
+        renderNode(nodeHistory.at(-1));
+    }
 }
 
 const node2 = new StoryNode(`You try to listen for any danger but rocks fall and everybody dies!`);
@@ -40,4 +57,4 @@ const node1 = new StoryNode(`You are super couragous, you die!`);
 const node0 = new StoryNode(
 `You get off your horse. Before you, you see the houses of <strong>Troutberk</strong>. The barber-surgeon sent you here to get petals of the local lilies growing around Trouberk's well. It's your father's last chance to cure his disease the barber said. The only problem, this village is touched by the curse of undeath. The gibbous moon threateningly hangs over you casting light down at your destination as if to mock you. The way ahead seems clear, the whole village that silent you could hear a needle drop. What do you intend to do next?`
 ,[new Choice("Venture ahead, courage is the way!",node1), new Choice("Take a good look around, better safe than sorry!",node2)]);
-renderNode(node0);
+nextNode(node0);
